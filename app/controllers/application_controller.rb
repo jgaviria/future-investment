75f0adb27608925d541fe1dde1a0c09bf9fe36d0
Juan Gaviria <jgaviria@clarityservices.com>
2019-05-24 10:30:02 -0400

    Tuning devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    properties_path
+  end
+
 end
+
+
