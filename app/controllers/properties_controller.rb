8044b5a124870014747f369f69310d5e17f9ea4f
Juan Gaviria <jgaviria@clarityservices.com>
2019-06-01 09:54:32 -0500

    Adding active storage for photos

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -86,7 +86,7 @@ class PropertiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def property_params
-      params.require(:property).permit(:address, :city, :state, :zip_code, :county, :owner_name, :auction_amount, :arv, :property_type, :number_of_bedrooms, :number_of_bathrooms, :home_sqr_footage, :property_sqr_footage, :found_by, :secondary_revision, :type_of_loan, :home_status, :notes, :agent, :review_by_date, :urgent, :possible_phone_numbers, :possible_address, :archive, :auction_type, :auction_date, :profit, :debts_attributes => [:id, :kind, :value])
+      params.require(:property).permit(:address, :city, :state, :zip_code, :county, :owner_name, :auction_amount, :arv, :property_type, :number_of_bedrooms, :number_of_bathrooms, :home_sqr_footage, :property_sqr_footage, :found_by, :secondary_revision, :type_of_loan, :home_status, :notes, :agent, :review_by_date, :urgent, :possible_phone_numbers, :possible_address, :archive, :auction_type, :auction_date, :profit, :property, :debts_attributes => [:id, :kind, :value])
     end
 
   def calculate_profit(params)
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,6 @@ class Property < ApplicationRecord
   accepts_nested_attributes_for :debts
 
   attr_accessor :p_profit
+
+  has_many_attached :property
 end
